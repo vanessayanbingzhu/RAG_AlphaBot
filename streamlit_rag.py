9106c95be96bcf6af7650ad6559f9cc9e7f7dd37
Vanessa <83655644+vanessayanbingzhu@users.noreply.github.com>
2024-03-06 15:38:05 -0500

    Update for UI
    -Update UI
    -Use MultiQueryRetriever

--- streamlit_rag.py
+++ streamlit_rag.py
@@ -1,7 +1,7 @@
- 
+# This code use MultiQuery chain 
+
 # section 1.Load needed packages functions
-__import__('pysqlite3')
-import sys
+ 
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
@@ -9,17 +9,18 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+from langchain.retrievers.multi_query import MultiQueryRetriever
 from dotenv import load_dotenv
 import os
 import time
 
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
+from ui.chat_ui import message_func, user_avatar, bot_avatar
+ 
 # section 2.setup OpenAI LLM and Embedding models
-
-# load_dotenv()
-# OPENAI_API_KEY  = os.getenv("openai_api_key")  
-os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+ 
+load_dotenv()
+OPENAI_API_KEY  = os.getenv("openai_api_key")  
+# os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 
 llm = OpenAI(temperature=0.01)
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002") 
@@ -35,49 +36,84 @@ embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
  
  
 # section 5. Create Streamlit front end
-
+ 
 def main():
     vectorstore = Chroma(persist_directory='./chromadb/', embedding_function=embeddings)
 
-    retriever = vectorstore.as_retriever(search_kwargs={'k':4})
-
-    
+    # retriever = vectorstore.as_retriever(search_kwargs={'k':4})
+    retriever=MultiQueryRetriever.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(search_type='mmr'),
+    )
+        
     # section 4. setup the Retriever Chain
     
-    qa = RetrievalQA.from_chain_type(llm = llm, chain_type='stuff',
-                                    retriever= retriever, return_source_documents=False)   
+    # qa = RetrievalQA.from_chain_type(llm = llm, chain_type='stuff',
+    #                                 retriever= retriever, return_source_documents=False)   
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type='stuff',
+        retriever=retriever,
+        return_source_documents=False
+        )
 
-    st.image('./assets/shih_tzu_bot.jpg',width=300)
-    st.title('🐶 Alpha Bot')
+    st.image('./assets/fintech-header.png')
+    st.title('📈🤖 Alpha Bot')
 
-    with st.sidebar:
+    # with st.sidebar:
         
-        st.image('./assets/big_banks_canada.jpg',width=300)
-        st.title('🐶 Alpha Bot')
-        st.caption("🚀 a dedicated bot for Toronto big 5 banks! ")
+        # st.image('./assets/big_banks_canada.jpg',width=300)
+        # st.title('🤖 Alpha Bot')
+        # st.caption("🚀 a dedicated bot for Toronto big 5 banks! ")
 
- 
+
+
+    with open("ui/sidebar.md", "r") as sidebar_file:
+        sidebar_content = sidebar_file.read()
+
+    with open("./ui/styles.md", "r") as styles_file:
+        styles_content = styles_file.read()
+
+    st.sidebar.markdown(sidebar_content)
+
+    st.write(styles_content, unsafe_allow_html=True)
+
+    gradient_text_html = """
+    <style>
+    .gradient-text {
+        font-weight: bold;
+        background: -webkit-linear-gradient(left, purple, blue);
+        background: linear-gradient(to right, purple, blue);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        display: inline;
+        font-size: 3em;
+    }
+    </style>
+    <div class="gradient-text">Alpha Bot</div>
+    """
+
+    st.markdown(gradient_text_html, unsafe_allow_html=True)
+    # Add a reset button
+    if st.sidebar.button("Reset Chat"):
+        for key in st.session_state.keys():
+            del st.session_state[key]
+
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
- 
-    for msg in st.session_state.messages:
-        if msg['role'] == 'user':
-            avatar = './assets/investor_avatar.jpg'
-        elif msg['role'] == 'assistant':
-            avatar = './assets/Alpha_avatar.jpg'
-        with st.chat_message(msg['role'], avatar=avatar):
-            st.markdown(msg['content'])
-
-
     if prompt := st.chat_input():
-        st.session_state.messages.append({'role':'user','avatar':'./assets/investor_avatar.jpg','content':prompt})
-        st.chat_message('user', avatar= './assets/investor_avatar.jpg').write(prompt)
+        st.session_state.messages.append({'role':'user','avatar':user_avatar,'content':prompt})
+        # st.chat_message('user', avatar= user_avatar).write(prompt)
 
         response = qa.invoke(prompt)
         msg = response['result']
-        st.session_state.messages.append({'role':"assistant", 'avatar':"./assets/Alpha_avatar.jpg",'content':msg})
-        st.chat_message("assistant", avatar = './assets/Alpha_avatar.jpg').write(msg)
+        st.session_state.messages.append({'role':"assistant", 'avatar':bot_avatar,'content':msg})
+        # st.chat_message("assistant", avatar = bot_avatar).write(msg)
+ 
+    for msg in st.session_state.messages:
+        message_func(msg)
+
 
 if __name__=='__main__':
     main()
