15d059679caf0105d0792dd6d75da6b5f55c85c6
Vanessa <83655644+vanessayanbingzhu@users.noreply.github.com>
2024-03-06 15:32:22 -0500

    Adding UI design

---
+++ ui/chat_ui.py
@@ -0,0 +1,70 @@
+import html
+import re
+
+import streamlit as st
+
+root_url="https://github.com/vanessayanbingzhu/RAG_AlphaBot/tree/main/assets"
+user_avatar='https://preview.redd.it/d1nhcalbqn1b1.png?width=511&format=png&auto=webp&s=7084cb3777d628927cb623e735c9a0d2de5c5bf7'
+bot_avatar='https://upload.wikimedia.org/wikipedia/commons/a/a1/Greek_lc_alpha.svg'
+# user_avatar='assets/investor_avatar.jpg'
+# bot_avatar='assets/banks_canada.jpg'
+
+def format_message(text):
+    """
+    This function is used to format the messages in the chatbot UI.
+
+    Parameters:
+    text (str): The text to be formatted.
+    """
+    text_blocks = re.split(r"```[\s\S]*?```", text)
+    code_blocks = re.findall(r"```([\s\S]*?)```", text)
+
+    text_blocks = [html.escape(block) for block in text_blocks]
+
+    formatted_text = ""
+    for i in range(len(text_blocks)):
+        formatted_text += text_blocks[i].replace("\n", "<br>")
+        if i < len(code_blocks):
+            formatted_text += f'<pre style="white-space: pre-wrap; word-wrap: break-word;"><code>{html.escape(code_blocks[i])}</code></pre>'
+
+    return formatted_text
+
+def message_func(message):
+    """
+    This function is used to display the messages in the chatbot UI.
+
+    Parameters:
+    text (str): The text to be displayed.
+    """
+    text=message['content']
+    if message['role'] == 'user':
+            message_alignment = "flex-end"
+            message_bg_color = "linear-gradient(135deg, #00B2FF 0%, #006AFF 100%)"
+            avatar_class = "user-avatar"
+            st.write(
+            f"""
+                <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
+                    <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px;">
+                        {text} \n </div>
+                    <img src="{user_avatar}" class="{avatar_class}" alt="avatar" style="width: 40px; height: 40px;" />
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
+
+    elif message['role'] == 'assistant':
+        message_alignment = "flex-start"
+        message_bg_color = "#D3D3D3"
+        avatar_class = "bot-avatar"
+        # text = format_message(text)
+
+        st.write(
+            f"""
+                <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
+                    <img src="{bot_avatar}" class="{avatar_class}" alt="avatar" style="width: 30px; height: 30px;" />
+                    <div style="background: {message_bg_color}; color: black; border-radius: 20px; padding: 10px; margin-right: 5px; margin-left: 5px; max-width: 75%; font-size: 14px;">
+                        {text} \n </div>                    
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
---
+++ ui/sidebar.md
@@ -0,0 +1,16 @@
+# AlphaBot
+
+🤖 AlphaBot is designed to simplify the way you interact with and understand the vast amount of information presented in banks' financial earnings calls. This innovative tool is your personal analyst, breaking down financial jargon and data into understandable insights, making it easier for you to make informed decisions.
+
+## Features
+
+- **🚀 Earnings Call Summaries**: Get concise, easily digestible summaries of earnings calls from major banks.
+- **📈 Financial Performance Insights**: Understand at a glance how a bank is performing financially.
+- **💡 Q&A Interpretation**: Q&A session of an earnings call usually contains valuable insights. AlphaBot filters through these exchanges to bring you the most relevant information, clarifying management's perpective.
+
+## Get Started
+Sample questions you can try with AlphaBot:
+
+- Can you provide me some highlight information from RBC earnings call 2024Q1?
+- Can you summarize RBC's risk appetite in a few bullet points concisely?
+- What is the major challenges that RBC is facing based on 2024Q1 earnings call?
